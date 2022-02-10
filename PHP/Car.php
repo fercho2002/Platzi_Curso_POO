8be87ef8ddd056a00f2c6d444894605d6be871f8
LAPTOP-I8DPA09K\ferto <fertorresaavedra@gmail.com>
2022-02-10 14:41:36 -0500

    le hice los cambios q ue se nececitabam para python y java scrpt
    git status

---
+++ PHP/Car.php
@@ -0,0 +1,7 @@
+class Car{
+    public $id = integer;
+    public $license = string;
+    public $driver = string;
+    plublic $passegenger = string;
+
+}
---
+++ PHP/Route.php
@@ -0,0 +1,5 @@
+class Route{
+    public $id = integer;
+    public $start = float;
+    public $end = fload;
+}
